1cb9dc9a2b04cdfda89cf586eee96790a67e8891
brainox <macmartins081@gmail.com>
2021-08-21 20:38:02 +0100

    created the tab bar items

--- Spotify/TitleBarController.swift
+++ Spotify/TitleBarController.swift
@@ -9,8 +9,64 @@ import UIKit
 
 class TitleBarController: UIViewController {
     
+    var musicBarButtonItem: UIBarButtonItem!
+    var podCastBarButtonItem: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupNavBar()
+    }
+    
+    func setupNavBar() {
+        navigationItem.leftBarButtonItems = [musicBarButtonItem, podCastBarButtonItem]
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        musicBarButtonItem = makeBarButtonItem(text: "Music", selectior: #selector(musicTapped))
+        podCastBarButtonItem = makeBarButtonItem(text: "Podcasts", selectior: #selector(podcastTapped))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func makeBarButtonItem(text: String, selectior: Selector) -> UIBarButtonItem {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: selectior, for: .primaryActionTriggered)
+        
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .largeTitle).withTraits(traits: [.traitBold]),
+            NSAttributedString.Key.foregroundColor: UIColor.label
+        ]
+        
+        let attributedText = NSMutableAttributedString(string: text, attributes: attributes)
+        
+        button.setAttributedTitle(attributedText, for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 16)
+        
+        
+        let barButtonItem = UIBarButtonItem(customView: button)
+        return barButtonItem
+    }
+    
+    @objc func musicTapped() {
+        
+    }
+    
+    @objc func podcastTapped() {
+        
+    }
+}
+
+extension UIFont {
+    func withTraits(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        let descriptor = fontDescriptor.withSymbolicTraits(traits)!
+        return UIFont(descriptor: descriptor, size: 0)
+    }
+    
+    func bold() -> UIFont {
+        return withTraits(traits: .traitBold)
     }
 }
