3d342624cd1573db1c5ce45a44c1dedd8a26dbe8
brainox <macmartins081@gmail.com>
2021-08-21 20:39:41 +0100

    Created Container class

---
+++ Spotify/Container.swift
@@ -0,0 +1,16 @@
+//
+//  Container.swift
+//  Spotify
+//
+//  Created by Decagon on 21/08/2021.
+//
+
+import Foundation
+import UIKit
+
+class Container: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
