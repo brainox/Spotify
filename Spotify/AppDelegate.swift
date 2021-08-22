83d73f05d9757b4b0ccf7197151514772aea3daf
brainox <macmartins081@gmail.com>
2021-08-22 02:56:40 +0100

    changed system background color to spotifyBlack

--- Spotify/AppDelegate.swift
+++ Spotify/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .systemBackground
+        window?.backgroundColor = .spotifyBlack
         window?.makeKeyAndVisible()
         let navigationController = UINavigationController(rootViewController: TitleBarController())
         
